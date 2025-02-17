894db07d852f80575e94ab9adfb30418ee626ebf
Todd Dugan <tdugan@valuebasedtech.com>
2025-02-17 09:45:58 -0800

    map sst stages to aws profiles

--- sst.config.ts
+++ sst.config.ts
@@ -7,6 +7,17 @@ export default $config({
       removal: input?.stage === "production" ? "retain" : "remove",
       protect: ["production"].includes(input?.stage),
       home: "aws",
+      providers: {
+        aws: {
+          region: "us-west-2",
+          profile:
+            input.stage === "production"
+              ? "prod1"
+              : input.stage === "staging"
+                ? "non-prod1"
+                : "dev",
+        },
+      },
     };
   },
   async run() {},
