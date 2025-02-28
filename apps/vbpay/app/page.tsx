3495f44973d220ddb3fac9b43d4a9e3964aef28c
Todd Dugan <tdugan@valuebasedtech.com>
2025-02-28 15:41:42 -0800

    update sst dep

--- apps/vbpay/app/page.tsx
+++ apps/vbpay/app/page.tsx
@@ -1,11 +1,15 @@
+import { Resource } from "sst";
+
 import { Button } from "@workspace/ui/components/button";
 
-export default function Page() {
+export default async function Page() {
+  const dbName = Resource.VBPayDB.name;
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Hello World</h1>
         <Button size="sm">Button</Button>
+        <p>{dbName}</p>
       </div>
     </div>
   );
