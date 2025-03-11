7df86bf2890477061da8562a8bde44cc42f42b32
Todd Dugan <tdugan@valuebasedtech.com>
2025-03-11 15:01:52 -0700

    update home page to use standard layout

--- apps/vbpay/app/page.tsx
+++ apps/vbpay/app/page.tsx
@@ -1,18 +1,9 @@
-"use client";
+import { StandardLayout } from "@/components/standard-layout";
 
-import { signOut } from "aws-amplify/auth";
-
-import { Button } from "@workspace/ui/components/button";
-
-export default function Page() {
+export default async function Page() {
   return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <h1 className="text-2xl font-bold">Hello World</h1>
-        <Button size="sm" onClick={() => signOut()}>
-          Button
-        </Button>
-      </div>
-    </div>
+    <StandardLayout>
+      <h1>Home</h1>
+    </StandardLayout>
   );
 }
