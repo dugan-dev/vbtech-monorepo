7c7c1f31e1cd35f31a60cedd9c61ccaf0583dde6
Todd Dugan <tdugan@valuebasedtech.com>
2025-03-14 16:38:31 -0700

    add add payer form

---
+++ apps/vbpay/app/network/payers/components/add-payer-form/add-payer-form-schema.ts
@@ -0,0 +1,117 @@
+import * as z from "zod";
+
+import { validateStringIsUrl } from "@workspace/ui/lib/validateStringIsUrl";
+
+const AddPayerFormSchema = z.object({
+  payerType: z.string().refine(
+    (type) => {
+      return type !== "";
+    },
+    {
+      message: "Required",
+    },
+  ),
+  initPerfYr: z.string().refine(
+    (year) => {
+      return year !== "";
+    },
+    {
+      message: "Required",
+    },
+  ),
+  initPerfMo: z.string().refine(
+    (month) => {
+      return month !== "";
+    },
+    {
+      message: "Required",
+    },
+  ),
+  cmsId: z
+    .string()
+    .optional()
+    .transform((value) => (value === "" ? undefined : value))
+    .refine(
+      (cmsId) => {
+        if (cmsId) {
+          return cmsId.length === 5;
+        }
+        return true;
+      },
+      {
+        message: "Must be 5 characters.",
+      },
+    ),
+  marketingName: z.string().min(1, "Required").max(255),
+  legalName: z
+    .string()
+    .max(255)
+    .optional()
+    .transform((value) => (value === "" ? undefined : value)),
+  referenceName: z
+    .string()
+    .max(20)
+    .optional()
+    .transform((value) => (value === "" ? undefined : value)),
+  taxId: z
+    .string()
+    .optional()
+    .transform((value) => (value === "" ? undefined : value?.replace("-", "")))
+    .refine(
+      (taxId) => {
+        if (taxId) {
+          return taxId.length === 9;
+        }
+        return true;
+      },
+      {
+        message: "Must be 9 digits.",
+      },
+    ),
+  parentOrgName: z
+    .string()
+    .max(255)
+    .optional()
+    .transform((value) => (value === "" ? undefined : value)),
+  websiteUrl: z
+    .string()
+    .optional()
+    .or(z.literal(""))
+    .transform((value) => (value === "" ? undefined : value))
+    .refine(
+      (url) => {
+        if (url) {
+          return validateStringIsUrl(url);
+        }
+        return true;
+      },
+      {
+        message: "Must be a valid URL.",
+      },
+    ),
+});
+
+type AddPayerFormData = z.infer<typeof AddPayerFormSchema>;
+type AddPayerFormInput = z.input<typeof AddPayerFormSchema>;
+type AddPayerFormOutput = z.output<typeof AddPayerFormSchema>;
+
+const AddPayerFormDefaultValues: AddPayerFormInput = {
+  payerType: "",
+  initPerfYr: "",
+  initPerfMo: "",
+  cmsId: "",
+  marketingName: "",
+  legalName: "",
+  referenceName: "",
+  taxId: "",
+  parentOrgName: "",
+  websiteUrl: "",
+};
+
+export {
+  AddPayerFormDefaultValues,
+  AddPayerFormSchema,
+  type AddPayerFormData,
+  type AddPayerFormInput,
+  type AddPayerFormOutput,
+};
---
+++ apps/vbpay/app/network/payers/components/add-payer-form/add-payer-form.tsx
@@ -0,0 +1,184 @@
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@workspace/ui/components/card";
+import { Form } from "@workspace/ui/components/form";
+import { FormCombo } from "@workspace/ui/components/form/form-combo";
+import { FormInput } from "@workspace/ui/components/form/form-input";
+import { FormSubmitButton } from "@workspace/ui/components/form/form-submit-button";
+import { ScrollArea } from "@workspace/ui/components/scroll-area";
+import { formatTaxId } from "@workspace/ui/lib/formatTaxId";
+import { ComboItem } from "@workspace/ui/types/combo-item";
+
+import { PerfMonthLabels, PerfMonths } from "@/types/perf-month";
+import { PerfYears } from "@/types/perf-year";
+import { ErrorDialog } from "@/components/error-dialog";
+
+import { useAddPayerForm } from "../../hooks/use-add-payer-form";
+
+type props = {
+  onSuccess?: () => void;
+  payerTypes: ComboItem[];
+};
+
+export function AddPayerForm({ onSuccess, payerTypes }: props) {
+  const {
+    form,
+    onSubmit,
+    isInsertPayerPending,
+    isErrorDialogOpen,
+    errorMsg,
+    errorTitle,
+    closeErrorDialog,
+  } = useAddPayerForm({
+    onSuccess,
+  });
+
+  return (
+    <Form {...form}>
+      <ErrorDialog
+        open={isErrorDialogOpen}
+        onOpenChange={closeErrorDialog}
+        description={errorMsg}
+        title={errorTitle}
+      />
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+        <ScrollArea className="max-h-[90vh] overflow-y-auto pr-4">
+          <fieldset disabled={isInsertPayerPending} className="space-y-4 mb-4">
+            <div className="grid grid-cols-2 gap-4">
+              <Card>
+                <CardHeader>
+                  <CardTitle className="flex items-center gap-2">
+                    Basic Info
+                  </CardTitle>
+                  <CardDescription>
+                    Enter basic payer information below.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-4">
+                  <FormCombo
+                    control={form.control}
+                    name="payerType"
+                    label="Payer Type"
+                    isRequired
+                    comboItems={payerTypes}
+                  />
+                  <FormInput
+                    control={form.control}
+                    name="marketingName"
+                    label="Marketing Name"
+                    type="text"
+                    isRequired
+                  />
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader>
+                  <CardTitle className="flex items-center gap-2">
+                    Performance Period
+                  </CardTitle>
+                  <CardDescription>
+                    Enter initial performance period information below.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-4">
+                  <FormCombo
+                    control={form.control}
+                    name="initPerfYr"
+                    label="Initial Performance Year"
+                    isRequired
+                    comboItems={PerfYears.map((py) => ({
+                      label: py,
+                      value: py,
+                    }))}
+                  />
+                  <FormCombo
+                    control={form.control}
+                    name="initPerfMo"
+                    label="Initial Performance Month"
+                    isRequired
+                    comboItems={PerfMonths.map((pm) => ({
+                      label: PerfMonthLabels[pm],
+                      value: pm,
+                    }))}
+                  />
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader>
+                  <CardTitle className="flex items-center gap-2">
+                    Identification
+                  </CardTitle>
+                  <CardDescription>
+                    Enter payer identification information below.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-4">
+                  <FormInput
+                    control={form.control}
+                    name="cmsId"
+                    label="CMS ID"
+                    type="text"
+                  />
+                  <FormInput
+                    control={form.control}
+                    name="taxId"
+                    label="Tax ID"
+                    type="text"
+                    formatOnChange={(value) => formatTaxId(value)}
+                  />
+                  <FormInput
+                    control={form.control}
+                    name="legalName"
+                    label="Legal Name"
+                    type="text"
+                  />
+                  <FormInput
+                    control={form.control}
+                    name="referenceName"
+                    label="Acronym/Nickname"
+                    type="text"
+                  />
+                </CardContent>
+              </Card>
+              <Card>
+                <CardHeader>
+                  <CardTitle className="flex items-center gap-2">
+                    Organization
+                  </CardTitle>
+                  <CardDescription>
+                    Enter organization information below.
+                  </CardDescription>
+                </CardHeader>
+                <CardContent className="space-y-4">
+                  <FormInput
+                    control={form.control}
+                    name="parentOrgName"
+                    label="Parent Organization Name"
+                    type="text"
+                  />
+                  <FormInput
+                    control={form.control}
+                    name="websiteUrl"
+                    label="Website URL"
+                    type="text"
+                  />
+                </CardContent>
+              </Card>
+            </div>
+          </fieldset>
+        </ScrollArea>
+        <div className="border-t p-3 bg-background fixed bottom-0 left-0 right-0 shadow-lg">
+          <div className="container max-w-screen-2xl mx-auto flex justify-end gap-4">
+            <FormSubmitButton isSaving={isInsertPayerPending} />
+          </div>
+        </div>
+      </form>
+    </Form>
+  );
+}
