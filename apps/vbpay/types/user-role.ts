ad2416fd43d79c35b95deb053a78e396828ceb4a
Todd Dugan <tdugan@valuebasedtech.com>
2025-03-25 16:11:33 -0700

    fix user roles

--- apps/vbpay/types/user-role.ts
+++ apps/vbpay/types/user-role.ts
@@ -1,9 +1,9 @@
 import { z } from "zod";
 
 type UserRole =
-  | "view"
   | "edit"
   | "add"
+  | "upload"
   | "approve"
   | "read-files"
   | "write-files"
@@ -11,28 +11,29 @@ type UserRole =
   | "write-notifications";
 
 const UserRoles = [
-  "view",
   "edit",
   "add",
+  "upload",
   "approve",
   "read-files",
   "write-files",
   "read-notifications",
   "write-notifications",
 ] as const;
+
 const UserRolesBpo = [
-  "view",
   "edit",
   "add",
+  "upload",
   "read-files",
   "write-files",
   "read-notifications",
   "write-notifications",
 ] as const;
 const UserRolesPayerPayers = [
-  "view",
   "edit",
   "add",
+  "upload",
   "approve",
   "read-files",
   "write-files",
@@ -48,9 +49,9 @@ const UserRolesPayees = [
 ] as const;
 
 const UserRoleLabels = {
-  view: "View",
   edit: "Edit",
   add: "Add",
+  upload: "Upload",
   approve: "Approve",
   "read-files": "Read Files",
   "write-files": "Write Files",
