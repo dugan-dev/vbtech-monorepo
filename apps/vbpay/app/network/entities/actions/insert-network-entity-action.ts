769d8fe9283d86dc69635adcca94e05f00a6597b
Todd Dugan <tdugan@valuebasedtech.com>
2025-03-25 14:02:14 -0700

    restrict server-actions by userType and add role level verification

--- apps/vbpay/app/network/entities/actions/insert-network-entity-action.ts
+++ apps/vbpay/app/network/entities/actions/insert-network-entity-action.ts
@@ -5,6 +5,8 @@ import "server-only";
 import { revalidatePath } from "next/cache";
 import { z } from "zod";
 
+import { UserRole } from "@/types/user-role";
+import { UserType } from "@/types/user-type";
 import { newPubId } from "@/lib/nanoid";
 import { authedActionClient } from "@/lib/safe-action";
 
@@ -17,21 +19,53 @@ const insertEntityActionSchema = z.object({
   formData: AddNetworkEntityFormSchema,
 });
 
+const ALLOWED_USER_TYPES: UserType[] = ["bpo", "payers", "payer"];
+
+const REQUIRED_USER_ROLE: UserRole = "add";
+
 export const insertNetworkEntityAction = authedActionClient
-  .metadata({ actionName: "insertNetworkEntityAction" })
+  .metadata({
+    actionName: "insertNetworkEntityAction",
+    allowedTypes: ALLOWED_USER_TYPES,
+  })
   .schema(insertEntityActionSchema)
   .action(
     async ({
       parsedInput: { formData, payerPubId, revalidationPath },
       ctx,
     }) => {
+      // New PubId for the new Entity
       const pubId = newPubId();
 
+      // Get user auth data
+      const { userId, usersAppAttrs } = ctx;
+      const hasIds = usersAppAttrs.ids && usersAppAttrs.ids.length > 0;
+
+      if (!hasIds) {
+        throw new Error(
+          "User does not have permission to add an entity for this payer.",
+        );
+      }
+
+      const payerPermisssions = usersAppAttrs.ids?.find(
+        (id) => id.id === payerPubId,
+      );
+
+      if (
+        !payerPermisssions ||
+        payerPermisssions.userRoles.includes(REQUIRED_USER_ROLE)
+      ) {
+        throw new Error(
+          "User does not have permission to add an entity for this payer.",
+        );
+      }
+
+      // Insert the new Entity
       await insertNetworkEntity({
         input: formData,
         pubId,
         payerPubId,
-        userId: ctx.userId,
+        userId,
       });
 
       revalidatePath(revalidationPath);
