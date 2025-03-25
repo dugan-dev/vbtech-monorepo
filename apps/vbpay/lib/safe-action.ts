7745c80b439cbd790bf09ac3089bf55f8abe2369
Todd Dugan <tdugan@valuebasedtech.com>
2025-03-25 14:00:19 -0700

    add userType validation to server-actions if metadata has allowed list and return userAppAttrs

--- apps/vbpay/lib/safe-action.ts
+++ apps/vbpay/lib/safe-action.ts
@@ -1,9 +1,11 @@
 import "server-only";
 
-import { unauthorized } from "next/navigation";
-import { authenticatedUser } from "@/utils/amplify-server-utils";
+import { forbidden, unauthorized } from "next/navigation";
+import { getUserData } from "@/repos/user-repository";
 import { createSafeActionClient } from "next-safe-action";
 import z from "zod";
+
+import { UserTypeEnum } from "@/types/user-type";
 
 const unauthedActionClient = createSafeActionClient({
   defineMetadataSchema() {
@@ -30,6 +32,8 @@ const authedActionClient = createSafeActionClient({
   defineMetadataSchema() {
     return z.object({
       actionName: z.string(),
+      allowedTypes: z.array(UserTypeEnum).optional(),
+      adminOnly: z.boolean().optional(),
     });
   },
   // Can also be an async function.
@@ -46,11 +50,13 @@ const authedActionClient = createSafeActionClient({
     return e.message;
   },
 }).use(async ({ next, metadata }) => {
+  const { allowedTypes, adminOnly } = metadata;
   // get user auth data
-  const user = await authenticatedUser();
+  const { isAuthenticated, userId, usersAppAttrs, email, firstName, lastName } =
+    await getUserData();
 
   // throw error if no user id
-  if (!user) {
+  if (!isAuthenticated || !userId || !usersAppAttrs) {
     console.error(
       `Action error: ${metadata?.actionName}`,
       "User not authenticated.",
@@ -58,7 +64,27 @@ const authedActionClient = createSafeActionClient({
     unauthorized();
   }
 
-  return await next({ ctx: { userId: user.userId } });
+  // throw error if user not allowed
+  if (allowedTypes && !allowedTypes.includes(usersAppAttrs.type)) {
+    console.error(
+      `Action error: ${metadata?.actionName}`,
+      "User not authorized. User type not allowed.",
+    );
+    forbidden();
+  }
+
+  // throw error if user not admin
+  if (adminOnly && !usersAppAttrs.admin) {
+    console.error(
+      `Action error: ${metadata?.actionName}`,
+      "User not authorized. User not admin.",
+    );
+    forbidden();
+  }
+
+  return await next({
+    ctx: { userId: userId, usersAppAttrs, email, firstName, lastName },
+  });
 });
 
 export { authedActionClient, unauthedActionClient };
