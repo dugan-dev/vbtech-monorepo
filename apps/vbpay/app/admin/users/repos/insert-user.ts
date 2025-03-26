489d3d1a4df50cfd15efa146afd6fe3c2020dbc5
Todd Dugan <tdugan@valuebasedtech.com>
2025-03-26 16:14:16 -0700

    add insert-user repo

---
+++ apps/vbpay/app/admin/users/repos/insert-user.ts
@@ -0,0 +1,35 @@
+import "server-only";
+
+import { db } from "@workspace/db/database";
+
+type props = {
+  userId: string;
+  newUserId: string;
+  email: string;
+  firstName: string;
+  lastName: string;
+};
+
+export function insertUser({
+  userId,
+  newUserId,
+  email,
+  firstName,
+  lastName,
+}: props) {
+  const now = new Date();
+
+  return db
+    .insertInto("user")
+    .values({
+      userId: newUserId,
+      email,
+      firstName,
+      lastName,
+      createdBy: userId,
+      createdAt: now,
+      updatedBy: userId,
+      updatedAt: now,
+    })
+    .execute();
+}
