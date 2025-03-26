aa472e43ec37cb809aeb8f6a6f27b126621176c3
Todd Dugan <tdugan@valuebasedtech.com>
2025-03-26 16:16:03 -0700

    update user in sql while updating in cognito

--- apps/vbpay/app/admin/users/action/edit-user-action.ts
+++ apps/vbpay/app/admin/users/action/edit-user-action.ts
@@ -10,6 +10,7 @@ import { UserType } from "@/types/user-type";
 import { authedActionClient } from "@/lib/safe-action";
 
 import { UserFormSchema } from "../component/user-form/user-form-schema";
+import { updateUser } from "../repos/update-user";
 import { editUser } from "../repos/user-management-repository";
 
 const actionSchema = z.object({
@@ -21,24 +22,42 @@ const actionSchema = z.object({
 export const editUserAction = authedActionClient
   .metadata({ actionName: "editUserAction", adminOnly: true })
   .schema(actionSchema)
-  .action(async ({ parsedInput: { formData, userId, revalidationPath } }) => {
-    const appAttrs: UserAppAttrs = {
-      app: "VBPay",
-      admin: formData.admin,
-      super: formData.super,
-      type: formData.type as UserType,
-      ids: formData.ids.map((id) => ({
-        id: id.id,
-        userMode: id.userMode,
-        userRoles: id.userRoles,
-      })),
-    };
-    await editUser(
-      formData.email,
-      formData.firstName,
-      formData.lastName,
-      appAttrs,
-      userId,
-    );
-    revalidatePath(revalidationPath);
-  });
+  .action(
+    async ({ parsedInput: { formData, userId, revalidationPath }, ctx }) => {
+      // Create user attributes
+      const appAttrs: UserAppAttrs = {
+        app: "VBPay",
+        admin: formData.admin,
+        super: formData.super,
+        type: formData.type as UserType,
+        ids: formData.ids.map((id) => ({
+          id: id.id,
+          userMode: id.userMode,
+          userRoles: id.userRoles,
+        })),
+      };
+
+      await Promise.all([
+        // Update user in Cognito
+        editUser(
+          formData.email,
+          formData.firstName,
+          formData.lastName,
+          appAttrs,
+          userId,
+        ),
+
+        // Update user in database
+        updateUser({
+          userId: ctx.userId,
+          usersUserId: userId,
+          email: formData.email,
+          firstName: formData.firstName,
+          lastName: formData.lastName,
+        }),
+      ]);
+
+      // Revalidate page
+      revalidatePath(revalidationPath);
+    },
+  );
