604a269be13f26552fad6c8186003956a52153f4
Todd Dugan <tdugan@valuebasedtech.com>
2025-03-29 15:00:36 -0700

    add auth and license checks to home page

--- apps/vbpay/app/page.tsx
+++ apps/vbpay/app/page.tsx
@@ -1,6 +1,24 @@
+import { redirect, unauthorized } from "next/navigation";
+import { getVBPayLicense } from "@/repos/license-repository";
+import { Setup } from "@/routes";
+import { authenticatedUser } from "@/utils/amplify-server-utils";
+
 import { StandardLayout } from "@/components/standard-layout";
 
 export default async function Page() {
+  const [user, license] = await Promise.all([
+    authenticatedUser(),
+    getVBPayLicense(),
+  ]);
+
+  if (!user) {
+    return unauthorized();
+  }
+
+  if (!license) {
+    return redirect(Setup({}));
+  }
+
   return (
     <StandardLayout>
       <h1>Home</h1>
