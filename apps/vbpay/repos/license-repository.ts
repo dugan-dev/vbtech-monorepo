ba166e4bd00b4396567ef9c37a5cca336943b617
Todd Dugan <tdugan@valuebasedtech.com>
2025-03-29 15:02:51 -0700

    add license repo and caching

---
+++ apps/vbpay/repos/license-repository.ts
@@ -0,0 +1,49 @@
+import "server-only";
+
+import { unstable_cache as cache } from "next/cache";
+import { env } from "@/env/server";
+
+import { db } from "@workspace/db/database";
+
+const VBPAY_LICENSE_CACHE_KEY = "VBPayLicenseInfoCacheKey";
+const REVALIDATE_SECONDS = 600; // 10 minutes
+
+function getVBPayLicenseQry() {
+  return db
+    .selectFrom("vbpayLicense")
+    .select([
+      "type",
+      "fromDate",
+      "toDate",
+      "clientName",
+      "pocName",
+      "pocPhone",
+      "pocEmail",
+      "numPayers",
+      "features",
+    ])
+    .executeTakeFirst();
+}
+
+type VBPayLicense = NonNullable<Awaited<ReturnType<typeof getVBPayLicenseQry>>>;
+
+function getVBPayLicense() {
+  if (env.NODE_ENV !== "production") {
+    const license = getVBPayLicenseQry();
+    return license;
+  }
+
+  return cache(
+    () => {
+      const license = getVBPayLicenseQry();
+      return license;
+    },
+    [VBPAY_LICENSE_CACHE_KEY],
+    {
+      revalidate: REVALIDATE_SECONDS,
+      tags: [VBPAY_LICENSE_CACHE_KEY],
+    },
+  )();
+}
+
+export { getVBPayLicense, VBPAY_LICENSE_CACHE_KEY, type VBPayLicense };
