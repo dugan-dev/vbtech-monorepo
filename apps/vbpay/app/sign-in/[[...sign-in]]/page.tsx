72f91b66ec34aac7cf9cf46da3bcdb298fef35f3
Todd Dugan <tdugan@valuebasedtech.com>
2025-03-29 14:59:48 -0700

    add page level redirect if user signed in

--- apps/vbpay/app/sign-in/[[...sign-in]]/page.tsx
+++ apps/vbpay/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,6 +1,15 @@
+import { redirect } from "next/navigation";
+import { Home } from "@/routes";
+import { authenticatedUser } from "@/utils/amplify-server-utils";
+
 import { SignInCard } from "./components/sign-in-card";
 
 export default async function SignIn() {
+  const user = await authenticatedUser();
+
+  if (user) {
+    return redirect(Home({}));
+  }
   return (
     <main className="flex min-h-screen items-center justify-center">
       <SignInCard />
