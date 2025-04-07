90a19d28b75ca58d8a0a42288e5f0d4b97dc8f42
Todd Dugan <tdugan@valuebasedtech.com>
2025-04-07 15:00:18 -0700

    pass user selection data where needed

--- apps/vbpay/app/network/physicians/layout.tsx
+++ apps/vbpay/app/network/physicians/layout.tsx
@@ -1,7 +1,13 @@
 import { PropsWithChildren } from "react";
+import { getUserSelectionData } from "@/repos/user-repository";
 
 import { StandardLayout } from "@/components/standard-layout";
 
-export default function Layout({ children }: PropsWithChildren) {
-  return <StandardLayout>{children}</StandardLayout>;
+export default async function Layout({ children }: PropsWithChildren) {
+  const userSelectionData = await getUserSelectionData();
+  return (
+    <StandardLayout userSelectionData={userSelectionData}>
+      {children}
+    </StandardLayout>
+  );
 }
