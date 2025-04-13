39ac025e6437c780ec89195416fc6e46cc028854
Todd Dugan <tdugan@valuebasedtech.com>
2025-04-12 23:16:05 -0700

    fix page naming for consistency

--- apps/vbpay/app/sign-in/[[...sign-in]]/page.tsx
+++ apps/vbpay/app/sign-in/[[...sign-in]]/page.tsx
@@ -1,14 +1,14 @@
 import { redirect } from "next/navigation";
-import { Home, SignIn as SignInRoute } from "@/routes";
+import { Home, SignIn } from "@/routes";
 import { authenticatedUser } from "@/utils/amplify-server-utils";
 import { checkPageRateLimit } from "@/utils/check-page-rate-limit";
 
 import { SignInCard } from "./components/sign-in-card";
 
-export default async function SignIn() {
+export default async function Page() {
   const [user] = await Promise.all([
     authenticatedUser(),
-    checkPageRateLimit({ pathname: SignInRoute({}) }),
+    checkPageRateLimit({ pathname: SignIn({}) }),
   ]);
 
   if (user) {
