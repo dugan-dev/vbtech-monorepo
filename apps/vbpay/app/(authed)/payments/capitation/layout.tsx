779dc7df0a5a23c9a0c0a97fdaac49027314e512
Todd Dugan <tdugan@valuebasedtech.com>
2025-04-23 20:33:55 -0700

    fix routes from rebase

--- apps/vbpay/app/(authed)/payments/capitation/layout.tsx
+++
@@ -1,7 +0,0 @@
-import { PropsWithChildren } from "react";
-
-import { StandardLayout } from "@/components/standard-layout";
-
-export default function Layout({ children }: PropsWithChildren) {
-  return <StandardLayout>{children}</StandardLayout>;
-}
