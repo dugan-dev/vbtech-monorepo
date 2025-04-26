6ab39abed660acb2b63c7b6b3205c395d1b68ad3
Todd Dugan <tdugan@valuebasedtech.com>
2025-04-25 22:36:41 -0700

    add services and solutions pages/routes

---
+++ apps/web/app/services/consulting/page.info.ts
@@ -0,0 +1,6 @@
+import { z } from "zod";
+
+export const Route = {
+  name: "ServicesConsulting",
+  params: z.object({}),
+};
---
+++ apps/web/app/solutions/payments/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="mt-20 flex flex-col items-center flex-1">
+      Provider Payment and Remittance Solution Page
+    </div>
+  );
+}
