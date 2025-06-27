e8e4ca67f35971d7703573ddc04d12b9afc366d3
Todd Dugan <tdugan@valuebasedtech.com>
2025-06-27 16:01:23 -0700

    fix theme for armare

--- apps/web-armare/components/providers.tsx
+++ apps/web-armare/components/providers.tsx
@@ -7,10 +7,9 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextThemesProvider
       attribute="class"
-      defaultTheme="system"
-      enableSystem
+      defaultTheme="light"
+      themes={["light"]}
       disableTransitionOnChange
-      enableColorScheme
     >
       {children}
     </NextThemesProvider>
